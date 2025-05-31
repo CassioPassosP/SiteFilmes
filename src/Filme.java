c0177d48d4d6e0ed99e383ae6ad1e76d44089429
Cassio Passos <passoscassio29@gmail.com>
2025-05-31 14:45:06 -0300

    POO - novas classes

---
+++ src/Filme.java
@@ -0,0 +1,9 @@
+public class Filme {    // filme com suas caracteristicas
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+}
+
